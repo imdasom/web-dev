c32e0ac2a1746fd0f388e7c121608a9e230a0e06
imdasom <imdasom101@gmail.com>
2019-09-12 17:09:25 +0900

    leetcode 046 (DONE), 344 (DONE)

--- js-algo/leetcode.046.js
+++ js-algo/leetcode.046.js
@@ -3,25 +3,28 @@
  * @return {number[][]}
  */
 const permute = function(nums) {
-
+  let resultList = [];
+  permutes(nums, 0, nums.length-1, new Array(nums.length), resultList);
+  return resultList;
 };
 
 /**
- * @param fixNum
  * @param permutationList
  * @param depth
  * @param goalDepth
+ * @param fixNumList
  */
-const permutations = function(fixNum, permutationList, depth, goalDepth) {
-  if(permutationList.length === 2) {
-    // 출력
-
-    return;
+const permutes = function(permutationList, depth, goalDepth, fixNumList, resultList) {
+  if(depth === goalDepth) {
+    fixNumList[depth] = permutationList[0];
+    resultList.push(fixNumList.slice(0));
   }
   for(let i=0; i<permutationList.length; i++) {
     const fixNum = permutationList[i];
-    const permutationList = permutationList.slice();
-    const depth = depth + 1;
-    permutations(fixNum, permutationList, depth, goalDepth);
+    const newPermutationList = permutationList.filter(value => value !== fixNum);
+    fixNumList[depth] = fixNum;
+    permutes(newPermutationList, depth + 1, goalDepth, fixNumList, resultList);
   }
 };
+
+console.log(permute([0, 1, 2, 3]));
