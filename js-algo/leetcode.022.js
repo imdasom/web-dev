cec8d7d7fbbfa2a49dfd0ceaf5c4c9c1cccadad3
imdasom <imdasom101@gmail.com>
2019-09-25 23:34:59 +0900

    leetcode 022 (DONE)

--- js-algo/leetcode.022.js
+++ js-algo/leetcode.022.js
@@ -3,19 +3,40 @@
  * @return {string[]}
  */
 const generateParenthesis = function(n) {
-  let resultList = [];
-  const defaultSize = n-1;
-  let bracketList = [...Array.from(Array(defaultSize).keys()).map(v=>'('), ...Array.from(Array(defaultSize).keys()).map(v=>')')];
-  for(let i = 0; i < bracketList.length; i++) {
-    let newBracketList = bracketList.slice(0);
-    newBracketList.splice(i, 0, '()');
-    resultList.push(newBracketList.join(''));
+  function generateParenthesis(currentDepth, goalDepth, bracketList) {
+    let newBracketList = [];
+    bracketList.forEach(bracket => {
+      for(let i = 0; i <= bracket.length; i++) {
+        const newBracket = insertBracket(bracket, i, '()');
+        newBracketList.push(newBracket);
+      }
+    });
+    if(currentDepth < goalDepth - 1) {
+      newBracketList = [...new Set(newBracketList)];
+      newBracketList = generateParenthesis(currentDepth + 1, goalDepth, newBracketList);
+    }
+    return [...new Set(newBracketList)];
   }
-  let newBracketList = bracketList.slice(0);
-  newBracketList.push('()');
-  resultList.push(newBracketList.join(''));
-  resultList.push([...Array.from(Array(n).keys()).map(v=>'()')].join(''));
-  return [...new Set(resultList)];
+  function insertBracket(bracketStr, i, newBracketStr) {
+    if(i < 0) {
+      throw new Error('index cannot access. i : ' + i);
+    }
+    let bracketChars = bracketStr.split('');
+    if(bracketChars.length < i) {
+      bracketChars.push(newBracketStr);
+      return bracketChars.join('');
+    } else {
+      bracketChars.splice(i, 0, newBracketStr);
+      return bracketChars.join('');
+    }
+  }
+  if(n === 1) {
+    return ['()'];
+  }
+  const currentDepth = 1;
+  const goalDepth = n;
+  const bracketList = ['()'];
+  return generateParenthesis(currentDepth, goalDepth, bracketList);
 };
 
-console.log(generateParenthesis(4));
+// console.log(generateParenthesis(4));
