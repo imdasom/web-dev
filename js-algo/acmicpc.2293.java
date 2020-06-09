f9d64c683fbac3679f19d1ab400b242c070fc464
imdasom <imdasom101@gmail.com>
2020-06-09 21:49:40 +0900

    백준 : 2293 동전 1 (DONE)
    같은 로직인데 javascript로 풀면 메모리초과
    java로 풀면 성공..

---
+++ js-algo/acmicpc.2293.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line.split(" ")[0]);
+        int K = Integer.parseInt(line.split(" ")[1]);
+        int[]   cache = new int[K + 1];
+                cache[0] = 1;
+        String readLine = null;
+        while ((readLine = br.readLine()) != null) {
+            int coin = Integer.parseInt(readLine);
+            for (int k = coin; k <= K; k++) {
+                cache[k] = cache[k] + cache[k - coin];
+            }
+        }
+        br.close();
+        System.out.println(cache[K]);
+    }
+}
