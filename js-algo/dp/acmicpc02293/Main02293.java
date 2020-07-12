eaed2897556a8d034f1575b0f9c7b80ed44a322c
imdasom <imdasom101@gmail.com>
2020-07-12 20:21:46 +0900

    알고리즘 분류별 폴더 생성

--- js-algo/dp/acmicpc02293/Main02293.java
+++ js-algo/dp/acmicpc02293/Main02293.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+class Main02293 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
